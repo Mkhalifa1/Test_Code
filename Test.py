ca34478397472652d729393cb3070b229491dead
Mahmoud Khalifa Abdellatif Hamed <63808216+Mkhalifa1@users.noreply.github.com>
2024-01-30 23:25:35 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,17 +1,14 @@
-while True :
-    try:
-        user_input=int(input("Enter your number"))
-        result=10/user_input
-        print(result)
-    except ZeroDivisionError:
-       print("Please enter  a number not zero")
+try:
+    user_input=int(input("Enter your number"))
+    result=10/user_input
+    print(result)
+except ZeroDivisionError:
+    while True:
+       print("Please enter a number not zero")
        user_input=int(input("Enter your number"))
-       result=10/user_input
-       print(result)
-       
-    except ValueError:
-        print("Please enter a integer value not string")
-        user_input=int(input("Enter your number"))
-        result=10/user_input
-        print(result)
-     
+       if user_input==0:
+           print("Please enter a number not zero")
+           user_input=int(input("Enter your number"))
+       else :
+           result=10/user_input
+           print(result)
