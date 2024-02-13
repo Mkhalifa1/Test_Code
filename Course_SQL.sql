70914956061d156298060d160397c857612e9701
Mahmoud Khalifa Abdellatif Hamed <63808216+Mkhalifa1@users.noreply.github.com>
2024-02-13 18:38:01 +0200

    Update Course_SQL.sql

--- Course_SQL.sql
+++ Course_SQL.sql
@@ -10,3 +10,15 @@ st_email varchar(200) unique
 insert into Student (st_id,st_name,st_email) values (1004,'Ali','mk2745656@gmail.com')
 
 update student set st_gpa=4 where st_id=1004
+
+-- **Alter Table - Add Column:**
+ALTER TABLE my_table
+ADD COLUMN email VARCHAR(100);
+
+-- **Alter Table - Drop Column:**
+ALTER TABLE my_table
+DROP COLUMN age;
+
+-- **Alter Table - Alter/Modify Column:**
+ALTER TABLE my_table
+ALTER COLUMN name TYPE VARCHAR(60);
